9ed27e95eae46fec927b97c296686e39b146921f
Chandrashekr112 <142377213+Chandrashekr112@users.noreply.github.com>
2023-08-16 11:03:02 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,7 @@
-Input: num1 = 5, num2 = 3
-Output: 8
-Input: num1 = 13, num2 = 6
-Output: 19
+import calendar  
+# Enter the month and year  
+yy = int(input("Enter year: "))  
+mm = int(input("Enter month: "))  
+  
+# display the calendar  
+print(calendar.month(yy,mm)) 
